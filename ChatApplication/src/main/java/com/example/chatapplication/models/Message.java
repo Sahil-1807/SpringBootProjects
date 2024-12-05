d7c022bbd246fca68f8024ebe1aca21ab6818286
sahil <sahilsingh180799@gmail.com>
2024-12-06 00:22:06 +0530

    added code for chat application using web sockets

---
+++ ChatApplication/src/main/java/com/example/chatapplication/models/Message.java
@@ -0,0 +1,27 @@
+package com.example.chatapplication.models;
+
+public class Message {
+    String name;
+    String content;
+
+    public Message(String name, String content) {
+        this.name = name;
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
