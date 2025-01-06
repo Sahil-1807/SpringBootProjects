a20259da2cb2df27bd6309896f125d31184d0eb8
sahil <sahilsingh180799@gmail.com>
2025-01-06 22:29:19 +0530

    added initial project having database optimization using redis as in memory cache

---
+++ RedisOptimization/src/main/java/com/example/redisOptimization/RedisOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.redisOptimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisOptimizationApplication.class, args);
+	}
+
+}
---
+++ RedisOptimization/src/test/java/com/example/redisOptimization/RedisOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redisOptimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisOptimizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
